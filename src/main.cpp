098af11470ac274f5872be46c8a9de07e3090b63
Nathan Cho <nathancho@Nathans-MacBook-Air.local>
2022-04-18 21:56:53 -0700

    did stuff

--- src/main.cpp
+++ src/main.cpp
@@ -16,48 +16,66 @@ void pre_auton() {
   vexcodeInit();
 }
 
-double kP = 0, kI = 0, kD = 0;
-int error, prevError = 0, totalError = 0, derivative;
+struct {
+  double kP = 0.0;
+  double kI = 0.0;
+  double kD = 0.0;
+  int error = 0;
+  int prevError = 0;
+  int integral = 0;
+  int derivative = 0;
+  int setPoint = 200;
+} lateral;
 
-double turnKP = 0, turnKI = 0, turnKD = 0;
-int turnError, turnPrevError = 0, turnTotalError = 0, turnDerivative;
-
-int desiredValue = 200;
-int desiredTurnValue = 0;
+struct {
+  double kP = 0.0;
+  double kI = 0.0;
+  double kD = 0.0;
+  int error = 0;
+  int prevError = 0;
+  int integral = 0;
+  int derivative = 0;
+  int setPoint = 0;
+} turning;
 
 bool enablePID = false;
 
 int drivePID() {
   while(enablePID) {
     
-    int leftMotorPosition = FrontLeftMotor.position(degrees);
-    int rightMotorPosition = FrontRightMotor.position(degrees);
+    // Lateral Motion
+    // For determining distance from desired value.
+    int currentPosition = (LeftMotorGroup.position(degrees) + RightMotorGroup.position(degrees)) / 2;
 
-    int averagePosition = (leftMotorPosition + rightMotorPosition) / 2;
+    lateral.error = currentPosition - lateral.setPoint;   // Update the current lateral error. The goal is to have error equal 0
+    lateral.derivative = lateral.error - lateral.prevError;   // Update the lateral derivative. Compares current error to the error in previous iteration. Makes small adjustments based on that value.
+    lateral.integral += lateral.error;                     // Update the total lateral error. If the proportional component is consistantly lacking or overshooting, integral will grow very large.
+    
+    // Proportional + Derivative + Integral
+    double lateralMotorPower = (lateral.error * lateral.kP + lateral.derivative * lateral.kD + lateral.integral * lateral.kI);
 
-    error = averagePosition - desiredValue; 
-    derivative = error - prevError;
-    totalError += error; 
+    // Turning Motion
+    // Comparing left and right motor groups then comparing to desired difference of the two motor groups.
+    int turnDifference = LeftMotorGroup.position(degrees) - RightMotorGroup.position(degrees);
 
-    double lateralMotorPower = (error * kP + derivative * kD + totalError * kI);
+    turning.error = turnDifference  - turning.setPoint;   // Explanation given above.
+    turning.derivative = turning.error - turning.prevError;
+    turning.integral += turning.error; 
 
-    // Trungingejfbsdkjf
+    // Proportional + Derivative + Integral
+    double turnMotorPower = (turning.error * turning.kP + turning.derivative * turning.kD + turning.integral * turning.kI);
 
-    int turnDifference = leftMotorPosition - rightMotorPosition;
-
-    turnError = turnDifference  - desiredTurnValue; 
-    turnDerivative = turnError - turnPrevError;
-    turnTotalError += turnError; 
-
-    double turnMotorPower = (turnError * turnKP + turnDerivative * turnKD + turnTotalError * turnKI);
-
+    // Use determined values to spin motors.
     FrontRightMotor.spin(forward, lateralMotorPower - turnMotorPower, voltageUnits::volt);
     FrontLeftMotor.spin(forward, lateralMotorPower + turnMotorPower, voltageUnits::volt);
     BackLeftMotor.spin(forward, lateralMotorPower + turnMotorPower, voltageUnits::volt);
     BackRightMotor.spin(forward, lateralMotorPower - turnMotorPower, voltageUnits::volt);
 
-    prevError = error;
-    turnPrevError = turnError;
+    // Record this iteration's error for next interation's dervative value.
+    lateral.prevError = lateral.error;
+    turning.prevError = turning.error;
+
+    // Don't hog computing power.
     vex::task::sleep(20);
   }
 
